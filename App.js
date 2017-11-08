d54c2d0cad2500c78a329aa3418bb258ead9a67e
Miles Colman <mcolman@gmail.com>
2017-11-07 23:08:40 -0800

    first tab bar

--- App.js
+++ App.js
@@ -1,23 +1,53 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Platform, View, Text, StatusBar } from 'react-native'
+import { TabNavigator , StackNavigator, DrawerNavigator} from 'react-navigation'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+import DeckList from './components/DeckList'
+import { Constants } from 'expo'
+import { white, purple } from './utils/colors'
+
+function UdaciStatusBar ({ backgroundColor, ...props}) {
+  return (
+    <View style={{backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  )
+}
+
+const Tabs = TabNavigator({
+  Decks: {
+      screen: DeckList,
+      navigationOptions: {
+        tabBarLabel: "Decks",
+        tabBarIcon: ({tintColor }) => <MaterialCommunityIcons name='cards' size={30}  color={tintColor}/>
+      }
+  },
+},
+{
+  tabBarOptions: {
+    activeTintColor: Platform.OS === 'ios' ? purple : white,
+    showIcon: true,
+    style: {
+      height : 56,
+      backgroundColor: Platform.OS === 'ios' ? white : purple,
+      shadowColor: 'rgba(0, 0, 0, 0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1
+    }
+  }
+})
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View style={{flex: 1}}>
+        <UdaciStatusBar backgroundColor={purple} barStyle='light-content'/>
+        <Tabs />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
