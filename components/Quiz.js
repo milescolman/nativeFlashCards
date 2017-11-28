6ef6f9b62ebf364cd40c31f1508c1a386868d973
Miles Colman <mcolman@gmail.com>
2017-11-28 13:09:10 -0800

    add flip card to Quiz question/answer display

--- components/Quiz.js
+++ components/Quiz.js
@@ -1,11 +1,53 @@
 import React, { Component } from 'react'
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet, Animated } from 'react-native'
 
 export default class Quiz extends Component {
   state = {
     questionIdx: 0,
     status: 'showQuestion',
     showAnswer: false,
+  }
+
+  componentWillMount() {
+    this.animatedValue = new Animated.Value(0)
+    this.value = 0
+    this.animatedValue.addListener(({ value }) => {
+      this.value = value
+    })
+    this.frontInterpolate = this.animatedValue.interpolate({
+      inputRange: [0, 180],
+      outputRange: ['0deg', '180deg'],
+    })
+    this.backInterpolate = this.animatedValue.interpolate({
+      inputRange: [0, 180],
+      outputRange: ['180deg', '360deg'],
+    })
+    this.backOpacity = this.animatedValue.interpolate({
+      inputRange: [89, 90],
+      outputRange: [0, 1],
+    })
+    this.frontOpacity = this.animatedValue.interpolate({
+      inputRange: [89, 90],
+      outputRange: [1, 0],
+    })
+  }
+  flipCard ()  {
+    // this.setStrrate((state) => ({
+    //   ...state,
+    //   showAnswer: !state.showAnswer,
+    // }))
+    if (this.value >= 90) {
+      Animated.timing(this.animatedValue, {
+        toValue: 0,
+        duration: 800
+      }).start()
+    } else {
+      Animated.timing(this.animatedValue, {
+        toValue: 180,
+        duration: 800
+      }).start()
+    }
+
   }
 
   render () {
@@ -21,37 +63,60 @@ export default class Quiz extends Component {
         width: 150,
         alignItems: 'center',
         margin: 5,
+      },
+      flipCardBack: {
+        position: 'absolute',
+        top: 0
+      },
+      frontAnimatedStyle: {
+        transform: [
+          { rotateY: this.frontInterpolate}
+        ],
+        // opacity: this.frontOpacity,
+      },
+      backAnimatedStyle: {
+        transform: [
+          { rotateY: this.backInterpolate}
+        ],
+        // opacity: this.backOpacity
       }
     })
     const { navigate } = this.props.navigation
     const { state } = this.props.navigation
 
     return (
-      <View style={{flex: 1}}>
+      <View style={{flex: 1, backgroundColor: 'white'}}>
         <View style={{flex: 1}}>
           <Text>
             2 / 2 to be replaced with progress through quiz
           </Text>
         </View>
-        <View style={styles.alignment}>
-          <View style={{alignItems: 'center'}}>
-            <Text style={{fontSize: 30, textAlign: 'center'}}>
-              {this.state.showAnswer ?
-                ('The answer from the store') :
-                (' A question from the  store?') }
-            </Text>
-            <TouchableOpacity
-              onPress={() => this.setState((state) => ({
-                ...state,
-                showAnswer: !state.showAnswer,
-              }))}
-            >
-              <Text style={{color: 'red'}}>
-                {this.state.showAnswer ?
-                  ('Answer') :
-                  ('Question') }
+        <View style={[styles.alignment, ]}>
+          <View>
+            <Animated.View style={[styles.frontAnimatedStyle, {alignItems: 'center', backfaceVisibility: 'hidden', backgroundColor: 'white', opacity: this.frontOpacity,}]}>
+              <Text style={{fontSize: 30, textAlign: 'center'}}>
+                'The answer from the store'
               </Text>
-            </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => this.flipCard()}
+              >
+                <Text style={{color: 'red'}}>
+                  Question
+                </Text>
+              </TouchableOpacity>
+            </Animated.View>
+            <Animated.View style={[styles.backAnimatedStyle, styles.flipCardBack, {alignItems: 'center', backfaceVisibility: 'hidden', backgroundColor: 'white', opacity: this.backOpacity}]}>
+              <Text style={{fontSize: 30, textAlign: 'center'}}>
+                'The question from the store'
+              </Text>
+              <TouchableOpacity
+                onPress={() => this.flipCard()}
+              >
+                <Text style={{color: 'red'}}>
+                  Answer
+                </Text>
+              </TouchableOpacity>
+            </Animated.View>
           </View>
           <View>
             <TouchableOpacity
